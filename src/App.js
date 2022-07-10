c403d2b23420061cdd6a574bbae6095d92b20c3d
nathyylad <35748432+nathyylad@users.noreply.github.com>
2022-07-10 10:36:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+function App() {
+  return (
+   
+     
+        <Button>test</Button>
+  
+  );
+}
+
+export default App;
